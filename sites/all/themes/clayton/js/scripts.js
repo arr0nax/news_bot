29fe556fff08d84a2e4f85092951946e933fd7fd
Clayton Collins <claytonccollins@gmail.com>
2017-05-17 11:43:50 -0700

    mess around with sentence optimization and session sites

--- sites/all/themes/clayton/js/scripts.js
+++ sites/all/themes/clayton/js/scripts.js
@@ -1,18 +1,28 @@
 
-jQuery(document).ready(function($) {
-  $('a').click(function(e) {
-    console.log(e.target);
-    var url = $(this).attr('href');
-    $.ajax({
-      type: 'POST',
-      url: '/redirect',
-      dataType: 'json',
-      success: ajaxCompleted,
-      data: 'url='+url
-    });
-  });
-
-  ajaxCompleted = function(data) {
-    console.log(data);
-  }
-});
+// jQuery(document).ready(function($) {
+//   $('a').click(function(e) {
+//     e.preventDefault();
+//     console.log(e.target);
+//     var url = $(this).attr('href');
+//     $.ajax({
+//       type: 'POST',
+//       url: '/redirect',
+//       dataType: 'json',
+//       success: ajaxCompleted,
+//       data: 'url='+url
+//     });
+//   });
+//
+//   ajaxCompleted = function(data) {
+//     console.log(data);
+//     // if (data = '/which_website') {
+//       // window.location.href = 'localhost:8888/which_website';
+//     // } else if (data = '/') {
+//       // window.location.href = 'localhost:8888';
+//     // } else {
+//     setTimeout(function() {
+//       location.reload();
+//     }, 1000);
+//     // }
+//   };
+// });
